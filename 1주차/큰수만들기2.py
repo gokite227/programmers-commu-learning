2883183051737f6f087c1760b5b3d4e0255c63ac
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-01-18 02:27:58 +0900

    큰수만들기2차

---
+++ 1주차/큰수만들기2.py
@@ -0,0 +1,15 @@
+def solution(number, k):
+    answer = ''
+    number = [str(x) for x in number]
+    num = [number[0]]
+    for i in range(1,len(number)) :
+        
+        if number[i] > number[i-1] and k > 0 :
+                del num[len(num)-1]
+                k = k -1
+                num.append(number[i])
+        else :   
+            num.append(number[i])
+        
+    print(num)
+    return answer
