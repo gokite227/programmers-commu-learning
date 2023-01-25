1f40c5061a2b12a104e0d038640f8a0c43402078
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-01-25 20:46:41 +0900

    더맵게1차

---
+++ 3주차/더 맵게.py
@@ -0,0 +1,19 @@
+def solution(scoville, K):
+    answer = 0
+    s_coville = scoville
+    f = 0
+    s = 0
+    for i in scoville :
+        print(i)
+        if len(scoville) == 1 and scoville[0] < k :
+            answer = -1
+            break
+        if i < K :
+            answer += 1
+            f = min(scoville)
+            scoville.remove(f)
+            s = min(scoville)
+            scoville.remove(s)
+            scoville.append(f + (s*2))
+    
+    return answer
